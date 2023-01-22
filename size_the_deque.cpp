7525c9b39be25d1a3d8c6232c29d2afb07a4b17a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-22 09:50:10 +0600

    Find the size of the deque

---
+++ size_the_deque.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    deque<int>de;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        if(a&1)
+        {
+            de.push_back(a);
+            de.push_front(a-2);
+        }
+    }
+    cout<<de.size()<<endl;
+}
