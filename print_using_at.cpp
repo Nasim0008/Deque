5d37b503d47ac83e288bf2aa019c39f5d446bdf1
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-23 23:13:33 +0600

    print all the elements from deque

---
+++ print_using_at.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    deque<int>de;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        if(a>5)
+        {
+            de.push_back(a);
+        }
+        else
+        {
+            de.push_front(a);
+        }
+    }
+    for(int i=0;i<de.size();i++)
+    {
+        cout<<de.at(i)<< " ";
+    }
+    cout<<endl;
+}
