3cbd9b756723b1dc1b60ff97ada13b1701bd2df6
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-23 23:34:28 +0600

    erase any single value from the deque

---
+++ erase_single_element.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    deque<int>de;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        if(a>5)
+        {
+            de.push_back(a);
+        }
+        else
+        {
+            de.push_front(a);
+        }
+    }
+
+    deque<int>::iterator it;
+    for(auto it=de.begin();it!=de.end();it++)
+    {
+        cout<< *it<< " ";
+    }
+    cout<<endl;
+    it = de.begin()+2;
+    cout<<*it<<endl;
+    de.erase(it);
+     for(auto it=de.begin();it!=de.end();it++)
+    {
+        cout<< *it<< " ";
+    }
+}
